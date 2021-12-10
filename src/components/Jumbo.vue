e52c0f101222262a49bae386262e49de11a890d6
Guido <ppapaguido@gmail.com>
2021-12-10 15:48:51 +0100

    prove varie con due diversi components

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img src="https://picsum.photos/800/400" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jumbo",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Titolo innovativo e bellissimo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
